06cd7b78507c72978687fffb20b15e0df31b9a2f
Kateryna <8039690+kateryna-ruzhytska@users.noreply.github.com>
2024-04-10 15:17:37 +0300

    Add files via upload

---
+++ MSHTML.py
@@ -0,0 +1,11 @@
+from enum import IntFlag
+
+import comtypes.gen._3050F1C5_98B5_11CF_BB82_00AA00BDCE0B_0_4_0 as __wrapper_module__
+from comtypes.gen._3050F1C5_98B5_11CF_BB82_00AA00BDCE0B_0_4_0 import 
+
+
+
+
+
+__all__ = []
+
